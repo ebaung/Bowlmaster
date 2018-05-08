2702846e7638c3dc02625014e888047d8dcf036b
Eugene Baung <ebaung@gmail.copm>
2018-05-08 21:53:37 +0900

    Realtime Bowling Scoring Solution

--- Assets/Scripts/ScoreMaster.cs
+++ Assets/Scripts/ScoreMaster.cs
@@ -20,7 +20,25 @@ public class ScoreMaster {
 	public static List<int> ScoreFrames (List<int>rolls){
 		List<int> frameList = new List<int> ();
 
-		// your code here
+        for (int i = 1; i < rolls.Count; i += 2){
+            if (frameList.Count == 10) { break;}    // Prevents 11th frame score
+
+            if (rolls[i-1] + rolls[i] < 10)     // Normal "open" frame
+            {
+                frameList.Add(rolls[i - 1] + rolls[i]);
+            }
+
+            if (rolls.Count - i <= 1) { break; }    // Insufficient look-ahead
+
+            if (rolls[i - 1] == 10)                 // STRIKE
+            {
+                i--;                                // Strike frame has just one bowl
+                frameList.Add(10 + rolls[i + 1] + rolls[i + 2]);
+            } else if (rolls[i - 1] + rolls[i] == 10)     // Calculate SPARE bonus
+            {
+                frameList.Add(10 + rolls[i + 1]);
+            }
+        }
 
 		return frameList;
 	}
